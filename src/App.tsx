1b6b65c595a52fc054b29d360b924ed7e3ead7da
Maya-Mostafa <mayamostafa@gmail.com>
2021-10-28 10:15:16 -0400

    referencing signin/signup components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import MenuBar from "./components/MenuBar";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 
 function App() {
   return (
     <div>
       <MenuBar />
+      {/* <SignIn/> */}
+      <SignUp/>
     </div>
   );
 }
