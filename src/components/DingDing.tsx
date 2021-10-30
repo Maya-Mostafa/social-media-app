271d40cdbfc27c14c34485e3c9c0f3722711fd07
Maya-Mostafa <mayamostafa@gmail.com>
2021-10-30 08:29:09 -0400

    adding DingDong component

---
+++ src/components/DingDing.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import Button from "@mui/material/Button";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CommentIcon from "@mui/icons-material/Comment";
+import classes from "../styles/DingDong.module.css";
+
+const DingDing = () => {
+  return (
+    <Card sx={{ minWidth: 275 }} className={classes.ddCntnr}>
+      <CardHeader
+        avatar={<Avatar aria-label="recipe">R</Avatar>}
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+          <span className={classes.notifText}>0 likes</span>
+        </IconButton>
+        <IconButton aria-label="share">
+          <CommentIcon />
+          <span className={classes.notifText}>3 comments</span>
+        </IconButton>
+        <IconButton aria-label="share" className={classes.deleteDD}>
+          <DeleteIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default DingDing;
