18c55a9f97257995d30e081e5b5952c3cfe7f90d
Maya-Mostafa <mayamostafa@gmail.com>
2021-11-01 06:30:07 -0400

    home adjust

--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { Container, Grid } from "@mui/material";
-import DingDing from "./DingDing";
 import MyProfile from "./MyProfile";
 import classes from "../styles/HomeLayout.module.css";
+import NewDingDing from "./NewDingDing";
+import DingDings from "./DingDings";
 
 const HomeLayout = () => {
-  return (
-    <Container component="main" maxWidth={"lg"} className={classes.homeCntnr}>
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item xs={12} lg={8}>
-          <DingDing />
-          <DingDing />
-          <DingDing />
-          <DingDing />
-        </Grid>
-        <Grid item xs={12} lg={4}>
-          <MyProfile/>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+	return (
+		<Container
+			component='main'
+			maxWidth={"lg"}
+			className={classes.homeCntnr}
+		>
+			<Grid container spacing={2} justifyContent='center'>
+				<Grid item xs={12} lg={8}>
+					<NewDingDing />
+					<DingDings />
+				</Grid>
+				<Grid item xs={12} lg={4}>
+					<MyProfile />
+				</Grid>
+			</Grid>
+		</Container>
+	);
 };
 
 export default HomeLayout;
- 
